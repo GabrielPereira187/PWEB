09430fed05519063418074b9f671c2de6b230b70
Gabriel.Pereira <61756615+GabrielPereira187@users.noreply.github.com>
2021-04-26 12:36:30 -0300

    Add files via upload

---
+++ Atividade10/janela.js
@@ -0,0 +1,17 @@
+function abreJanela(){
+   
+    let Janela = document.getElementById('Janela');
+    Janela.src = "janelaaberta.png";
+}
+
+function fechaJanela(){
+   
+    let Janela = document.getElementById('Janela');
+    Janela.src = "janelafechada.png";
+}
+
+function quebraJanela(){
+    
+    let Janela = document.getElementById('Janela');
+    Janela.src = "janelaquebra.png";
+}
